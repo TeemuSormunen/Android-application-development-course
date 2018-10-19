f064652eb996b5c54bb425a21e0259041422f3d1
K9414(Teemu) <k9414@student.jamk.fi>
2018-10-19 17:45:56 +0300

    add basics ui exercise 1

---
+++ BasicsUiControl/app/src/main/java/com/example/teemusormunen/basiccs_ui_controls/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.teemusormunen.basiccs_ui_controls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void selectButtonClicked(View view) {
+// find radiogroup
+        RadioGroup rg = (RadioGroup) findViewById(R.id.myRadioGroup);
+        int id = rg.getCheckedRadioButtonId();
+// find button
+        RadioButton rb = (RadioButton) findViewById(id);
+// get radiobuttontext
+        String text = (String) rb.getText();
+// toast message to screen
+        Toast.makeText(getApplicationContext(), text,
+                Toast.LENGTH_SHORT).show();
+    }
+
+
+}
