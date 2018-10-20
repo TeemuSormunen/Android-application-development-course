fd1b4805472c4623ce9b98ed119d61dd87ad9441
K9414(Teemu) <k9414@student.jamk.fi>
2018-10-20 14:43:06 +0300

    add sum calculator

---
+++ SumCalculator/app/src/main/java/com/example/teemusormunen/sumcalculator/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.teemusormunen.sumcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void calculate(View view){
+        // Get numbers
+        EditText number1 = (EditText) findViewById(R.id.editText);
+        EditText number2 = (EditText) findViewById(R.id.editText2);
+        String numberOne = number1.getText().toString();
+        String numberTwo = number2.getText().toString();
+        // parse to int
+        int finalNumberOne = Integer.parseInt(numberOne);
+        int finalNumberTwo = Integer.parseInt(numberTwo);
+        int sum = finalNumberOne + finalNumberTwo;
+        // parse sum to string
+        String sumText = Integer.toString(sum);
+        // Toast sum
+        Toast.makeText(getApplicationContext(), sumText, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+}
