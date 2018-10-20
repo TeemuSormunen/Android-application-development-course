3a239cf3434c42ffab184014c2972e044000ccbc
K9414(Teemu) <k9414@student.jamk.fi>
2018-10-20 13:06:35 +0300

    add basics UI excercise 2

--- basicsUI2/app/src/main/java/com/example/teemusormunen/basicsui2/MainActivity.java
+++ basicsUI2/app/src/main/java/com/example/teemusormunen/basicsui2/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.teemusormunen.basicsui2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +16,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // AutoCompleteTextView
+        AutoCompleteTextView actv = (AutoCompleteTextView) findViewById(R.id.login);
+        // add stings to control
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,
+                new String[]{"Pasi", "Juha", "Kari", "Jouni", "Esa", "Hannu"});
+        actv.setAdapter(aa);
 
     }
-    // AutoCompleteTextView
-    AutoCompleteTextView actv = (AutoCompleteTextView)
-    findViewById(R.id.login);
-    // add stings to control
-    ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,
-                    new String[]{"Pasi", "Juha", "Kari", "Jouni", "Esa", "Hannu"});
-    actv.setAdapter(aa);
+
+    public void loginButtonClicked(View view){
+
+
+        // find login text
+        AutoCompleteTextView autoComp = (AutoCompleteTextView) findViewById(R.id.login);
+        String textLogin = autoComp.getText().toString();
+
+        // find password text
+        TextView passwrd = (TextView) findViewById(R.id.password);
+        String textPasswrd = passwrd.getText().toString();
+
+        // make toast
+        String text = textLogin + " " + textPasswrd;
+        Toast.makeText(getApplicationContext(), text,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
 
 
 }
