06bbf0628650562fa5bb4313d11ff68f7b2e898c
K9414(Teemu) <k9414@student.jamk.fi>
2018-10-20 15:33:16 +0300

    edit sum calculator

--- SumCalculator/app/src/main/java/com/example/teemusormunen/sumcalculator/MainActivity.java
+++ SumCalculator/app/src/main/java/com/example/teemusormunen/sumcalculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,9 +25,14 @@ public class MainActivity extends AppCompatActivity {
         int finalNumberTwo = Integer.parseInt(numberTwo);
         int sum = finalNumberOne + finalNumberTwo;
         // parse sum to string
+        TextView textView = (TextView) findViewById(R.id.textView);
         String sumText = Integer.toString(sum);
+        textView.setText(sumText);
+
+
+        //String sumText = Integer.toString(sum);
         // Toast sum
-        Toast.makeText(getApplicationContext(), sumText, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(), sumText, Toast.LENGTH_SHORT).show();
 
 
     }
