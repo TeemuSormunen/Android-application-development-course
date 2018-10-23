f178f2428774ee0bea71bed6a1348296d5dc2d94
K9414(Teemu) <k9414@student.jamk.fi>
2018-10-23 20:24:57 +0300

    modify detailActivity.java

--- ListView/app/src/main/java/com/example/teemusormunen/listview/DetailActivity.java
+++ ListView/app/src/main/java/com/example/teemusormunen/listview/DetailActivity.java
@@ -1,13 +1,37 @@
 package com.example.teemusormunen.listview;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // get intent which has used to open this activity
+        Intent intent = getIntent();
+        //get data from intent
+        Bundle bundle = intent.getExtras();
+        //get phone name
+        String phone = bundle.getString("phone");
+        // update text and image views to show data
+        TextView phoneTextView = (TextView) findViewById(R.id.phoneNameView);
+        phoneTextView.setText(phone);
+        ImageView phoneImageView = (ImageView) findViewById(R.id.phoneImage);
+        // show phone image
+        switch (phone) {
+            case "Android": phoneImageView.setImageResource(R.drawable.android); break;
+            case "iPhone": phoneImageView.setImageResource(R.drawable.ios); break;
+            case "WindowsMobile": phoneImageView.setImageResource(R.drawable.windows); break;
+            case "Blackberry": phoneImageView.setImageResource(R.drawable.blackberry); break;
+            case "WebOS": phoneImageView.setImageResource(R.drawable.webos); break;
+            case "Ubuntu": phoneImageView.setImageResource(R.drawable.ubuntu); break;
+        }
+
 
     }
 
