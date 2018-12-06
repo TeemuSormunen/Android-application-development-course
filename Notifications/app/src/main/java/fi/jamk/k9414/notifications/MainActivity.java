5ca74b4778196a74b491bf5fc02cbdac8b0db90b
K9414(Teemu) <k9414@student.jamk.fi>
2018-12-06 13:10:47 +0200

    add imageview example

--- Notifications/app/src/main/java/fi/jamk/k9414/notifications/MainActivity.java
+++ Notifications/app/src/main/java/fi/jamk/k9414/notifications/MainActivity.java
@@ -4,40 +4,48 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.os.Bundle;
-
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
+
+    public int notification_id = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int notification_id = 0;
-
-
-
 
     }
-
-    }
-    public void buttonPressed(){
+    public void notifyButtonPressed(View view) {
         // create new notification
-        Notification notification = new Notification.Builder(this)
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, "test")
                 .setCategory(Notification.CATEGORY_MESSAGE)
                 .setContentTitle("New message")
                 .setContentText("Have a nice day")
                 .setSmallIcon(R.drawable.ic_launcher_background)
-                .setVisibility(Notification.VISIBILITY_PUBLIC)
-                .build();
-
+                .setVisibility(Notification.VISIBILITY_PUBLIC).build();
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
+        Log.v("test", "test");
         // increase notification id
-        notification_id++;
+        notification_id += 1;
 
-        notificationManager.notify(notification_id, notification);
+
+            notificationManager.notify(notification_id, notification);
+
+
+
+
+    }
 
 
 }
+
+
+
+
+
