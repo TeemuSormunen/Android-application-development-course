0989785bccf9d34f6802cccd38a057a215377f1f
TeemuSormunen <teemu.sormunen@gmail.com>
2019-10-20 15:22:22 +0300

    Update Youtubeconfig.java

--- VideoPlayer/app/src/main/java/com/example/teemusormunen/videoplayer/Youtubeconfig.java
+++ VideoPlayer/app/src/main/java/com/example/teemusormunen/videoplayer/Youtubeconfig.java
@@ -2,7 +2,7 @@ package com.example.teemusormunen.videoplayer;
 
 public class Youtubeconfig {
     public Youtubeconfig(){}
-        private static final String API_KEY = "AIzaSyAZsy23uzOCOYRM2-j1IARHHPAWGsSaNpw";
+        private static final String API_KEY = "API_KEY";
 
     public static String getApiKey(){
         return API_KEY;
